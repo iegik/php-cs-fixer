e3ec80b04c379ff0048f8d6390ab9a6f97a173bf
Arturs Jansons <a.jansons@gmail.com>
2025-02-27 10:57:01 +0200

    Add workingDir config
    Issue: unable to find .php-cs file when pass "php-cs-fixer.configFile": ".php-cs", config.

--- extension.js
+++ extension.js
@@ -2,7 +2,7 @@ const vscode = require('vscode')
 const { exec } = require('child_process')
 
 function activate (context) {
-  if (vscode.window.activeTextEditor.document.languageId !== 'php') {
+  if (vscode.window.activeTextEditor?.document.languageId !== 'php') {
     return
   }
 
@@ -13,30 +13,43 @@ function activate (context) {
       return vscode.window.showErrorMessage('PHP-CS-Fixer: Path mode "intersection" requires a config file be set. Please set one or change path mode to "override".')
     }
 
+    // Get the active workspace folder
+    const workspaceFolders = vscode.workspace.workspaceFolders;
+    const workingDir = config.workingDir || workspaceFolders ? workspaceFolders[0].uri.fsPath : vscode.workspace.rootPath;
+
+    if (!workingDir) {
+        vscode.window.showErrorMessage("No workspace folder found!");
+        return;
+    }
+
     const param = []
 
     if (config.executable) {
       param.push(config.executable)
     }
 
-    param.push(`fix ${vscode.window.activeTextEditor.document.fileName} --using-cache=no`)
+    param.push(`fix ${vscode.window.activeTextEditor.document.fileName} --using-cache no`)
 
     if (config.configFile) {
-      param.push(`--config=${config.configFile}`)
+      param.push(`--config ${config.configFile}`)
     }
 
     if (config.pathMode) {
-      param.push(`--path-mode=${config.pathMode}`)
+      param.push(`--path-mode ${config.pathMode}`)
     }
 
     if (config.allowRisky) {
-      param.push('--allow-risky=yes')
+      param.push('--allow-risky yes')
     }
 
-    exec(param.join(' '), (err, stdout, stderr) => {
+    exec(param.join(' '), { cwd: workingDir }, (err, stdout, stderr) => {
       if (err) {
-        vscode.window.showErrorMessage('PHP-CS-Fixer: An error occurred.')
-        throw err
+        vscode.window.showErrorMessage(`PHP-CS-Fixer Error: ${err.message}`);
+        return;
+      }
+      if (stderr) {
+        vscode.window.showWarningMessage(`Warning: ${stderr}`);
+        return;
       }
     })
   }))
